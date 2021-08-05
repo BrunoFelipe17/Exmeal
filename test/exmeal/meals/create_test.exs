05ddc78d6c24a3d5181cdf8f40cd80d36150fa3c
BrunoFelipe17 <bruno.felipe7170@gmail.com>
2021-08-05 09:06:44 -0300

    Inserting User on DB

--- test/exmeal/meals/create_test.exs
+++ test/exmeal/meals/create_test.exs
@@ -4,7 +4,7 @@ defmodule Exmeal.Meals.CreateTest do
   describe "Create Meal" do
     test "when all params are valid, returns the meal" do
       params = %{
-        # calories: 20,
+        calories: 20,
         date: ~D[2001-05-02],
         description: "Banana"
       }
